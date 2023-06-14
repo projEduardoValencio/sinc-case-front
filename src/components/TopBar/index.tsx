5eb63630ea2d9afa83cad8ce895bf5aaeeaf311a
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2023-06-14 06:10:23 -0300

    feat: routes in home

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,6 +1,7 @@
 import { Flex, Text } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/image";
 import SideMenu from "./SideMenu";
+import Link from "next/link";
 
 const TopBar: React.FC = () => {
   return (
@@ -13,17 +14,30 @@ const TopBar: React.FC = () => {
       justify={["space-between"]}
       paddingX={["20px"]}
     >
-      <Flex className="logo" height={["100%"]} align={"center"} gap={["20px"]}>
-        <Image
-          src="/images/logo.png"
-          alt="suporte logo"
-          height={["70%"]}
-          width={"auto"}
-        />
+      <Flex className="logo" height={["100%"]} align={"center"}>
+        <Link
+          href={"/"}
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: "20px",
+            cursor: "pointer",
+          }}
+        >
+          <Image
+            src="/images/logo.png"
+            alt="suporte logo"
+            height={["70%"]}
+            width={"auto"}
+          />
 
-        <Text fontSize={["24px"]} color={"white"}>
-          CAR RENTAL
-        </Text>
+          <Text fontSize={["24px"]} color={"white"}>
+            CAR RENTAL
+          </Text>
+        </Link>
       </Flex>
 
       <SideMenu />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-import { Flex } from "@chakra-ui/layout";
+import { Flex, Text } from "@chakra-ui/layout";
 import TopBar from "@/components/TopBar";
+import { Divider, Image } from "@chakra-ui/react";
+import Link from "next/link";
 import RegisterCard from "@/components/RegisterCard";
 
 export default function Home() {
@@ -7,7 +9,65 @@ export default function Home() {
     <Flex width={"100vw"} height={"100vh"} direction={"column"}>
       <TopBar />
 
-      <RegisterCard />
+      <Flex
+        width={["100%"]}
+        height={["100%"]}
+        align={["center"]}
+        justify={["center"]}
+      >
+        <Flex
+          background={"primary"}
+          width={["50%"]}
+          height={["60%"]}
+          borderRadius={["40px"]}
+          direction={["column"]}
+          align={["center"]}
+          paddingX={["20px"]}
+          paddingY={["40px"]}
+          gap={["100px"]}
+        >
+          <Flex
+            direction={["column"]}
+            align={["center"]}
+            gap={["20px"]}
+            justify={["center"]}
+          >
+            <Text fontSize={["46px"]} fontWeight={["bold"]}>
+              CAR RENTAL
+            </Text>
+            <Image src="/images/logo.png" alt="logo suporte" width={["50%"]} />
+          </Flex>
+
+          <Flex
+            direction={["row"]}
+            fontSize={["34px"]}
+            gap={["90px"]}
+            align={["center"]}
+          >
+            <Link
+              className="nav-item-main"
+              href={"/register"}
+              style={{ textAlign: "center", width: "200px" }}
+            >
+              Registro
+            </Link>
+            <Link
+              className="nav-item-main"
+              href={"/view"}
+              style={{ textAlign: "center", width: "200px" }}
+            >
+              Listagem
+            </Link>
+            <Link
+              className="nav-item-main"
+              href={"/rental"}
+              style={{ textAlign: "center", width: "200px" }}
+            >
+              Locação
+            </Link>
+          </Flex>
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
